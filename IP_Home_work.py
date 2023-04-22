4ca333c1453ae5758b87b99f098940fe49501555
Kirill <mkir2006@gmail.com>
2023-04-22 10:18:19 +0300

    rework IP-address

--- IP_Home_work.py
+++ IP_Home_work.py
@@ -4,5 +4,4 @@ result = all(part.isdigit() and 0 <= int(part) <= 255 for part in ip_address) # 
 if result:
     print('Все числа со значением от 0 до 255')
 else:
-    any(not part.isdigit() or int(part) < 0 or int(part) > 255 for part in ip_address)
     print('Хотя бы одно число не со значением от 0 до 255')
